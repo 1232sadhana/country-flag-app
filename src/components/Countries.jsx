f6ad71b2d15f910ae3d747a31259e9a3d996c70b
SADHANA JADHAV <136025180+1232sadhana@users.noreply.github.com>
2024-02-20 16:50:12 +0000

    Test commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./country.css";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
 function Country() {
   const [countries, setCountries] = useState([]);
-  
-  //fetching data
+
+  // Fetching data
   async function fetchData() {
-    const response = await fetch("https://restcountries.com/v3.1/all");
-    const data = await response.json();
-    console.log(data);
-    setCountries(data);
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+     
+    }
   }
+
   useEffect(() => {
     fetchData();
   }, []);
-
-  
-  
 
   return (
     <>
       <h1>Country Flag</h1>
 
       <div className="country-container">
-        {records.map((country, idx) => (
+        {countries.map((country, idx) => (
           <div key={idx} className="country">
             <img
               src={country.flags.png}
@@ -35,9 +35,9 @@ function Country() {
           </div>
         ))}
       </div>
-
-      
     </>
   );
 }
+
 export default Country;
+
